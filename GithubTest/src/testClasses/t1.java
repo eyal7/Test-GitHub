a6322298fd650b76a13e6fadf3e5bda4244e4232
dell <dell@D1234567>
2015-08-16 21:45:53 +0300

    first checkin

---
+++ GithubTest/src/testClasses/t1.java
@@ -0,0 +1,25 @@
+package testClasses;
+
+public class t1 {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String wordT1="t1";
+		t2 tt2=new t2("Test",783);
+		t3 tt3=new t3(wordT1);
+		
+		runTest(wordT1,tt2,tt3);
+		
+	}
+
+	private static void runTest(String wordT1, t2 tt2, t3 tt3) {
+		System.out.println(wordT1+" = "+tt3.wordT3);
+		System.out.println();
+		System.out.println(tt2.wordT2+" "+tt2.num+"!!!!!!");
+		
+	}
+
+}
---
+++ GithubTest/src/testClasses/t2.java
@@ -0,0 +1,35 @@
+package testClasses;
+
+public class t2 {
+String wordT2;
+int num;
+
+public t2() {
+	this.wordT2 = "t2";
+	this.num = 0;
+}
+
+public t2(String wordT2, int num) {
+	super();
+	this.wordT2 = wordT2;
+	this.num = num;
+}
+
+public String getWordT2() {
+	return wordT2;
+}
+
+public void setWordT2(String wordT2) {
+	this.wordT2 = wordT2;
+}
+
+public int getNum() {
+	return num;
+}
+
+public void setNum(int num) {
+	this.num = num;
+}
+
+
+}
